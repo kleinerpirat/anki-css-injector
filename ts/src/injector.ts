24aa883574c50bc96ab08df7e8c3eee89c42634b
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-05-25 00:24:42 +0200

    use async generator to obtain editables
    Thanks to Hikaru Yoshiga!

--- ts/src/injector.ts
+++ ts/src/injector.ts
@@ -1,55 +1,40 @@
-import { shadowRoots } from "./utils";
-import * as NoteEditor from "anki/NoteEditor";
-import { tick } from "svelte/internal";
+import { iterateAnkiEditables } from "./utils";
 
 const StyleInjector = {
+    editables: <HTMLElement[]>[],
     initialized: false,
-    prevMid: "",
     addonPackage: "",
-    roots: <ShadowRoot[]>[],
     init: async (addonPackage: string): Promise<void> => {
-        return new Promise((resolve) => {
-            if (addonPackage) StyleInjector.addonPackage = addonPackage;
-            NoteEditor.lifecycle.onMount(async () => {
-                let ticks = 0;
-                while (!NoteEditor?.instances[0]?.fields[0]?.element && ticks < 10) {
-                    await tick();
-                    ticks++;
-                }
-                await StyleInjector.injectCSS();
-                StyleInjector.initialized = true;
-                resolve();
-            });
-        })
+        if (addonPackage) StyleInjector.addonPackage = addonPackage;
+
+        /* Thanks to Hikaru Yoshiga for kindly providing this generator! */
+        for await (const editable of iterateAnkiEditables()) {
+            StyleInjector.editables.push(editable);
+        }
+
+        await StyleInjector.injectCSS();
+        StyleInjector.initialized = true;
     },
     injectCSS: async (): Promise<void> => {
-        return new Promise(async (resolve) => {
-            StyleInjector.roots = await shadowRoots();
-            // timeout required because shadowRoots are not yet populated with editables
-            setTimeout(() => {
-                StyleInjector.roots.forEach((root, i) => {
-                    const editable = root.querySelector("anki-editable") as HTMLElement;
-    
-                    editable.classList.add(...document.body.classList);
-                    editable.setAttribute("ord", (i + 1).toString());
-                    editable.setAttribute(
-                        "field",
-                        root.host.closest(".editor-field")?.querySelector(".label-name")
-                            ?.innerHTML || "",
-                    );
-    
-                    if (!editable.hasAttribute("has-css-injected")) {
-                        const link = document.createElement("link");
-                        link.href = `/_addons/${StyleInjector.addonPackage}/user_files/field.css`;
-                        link.type = "text/css";
-                        link.rel = "stylesheet";
-                        root.insertBefore(link, editable);
-                        editable.setAttribute("has-css-injected", "");
-                    }
-                });
-                resolve();
-            });
-        })
+        StyleInjector.editables.forEach((editable, i) => {
+            const root = editable.getRootNode() as ShadowRoot;
+            editable.classList.add(...document.body.classList);
+            editable.setAttribute("ord", (i + 1).toString());
+            editable.setAttribute(
+                "field",
+                root.host.closest(".editor-field")?.querySelector(".label-name")
+                    ?.innerHTML || "",
+            );
+
+            if (!editable.hasAttribute("has-css-injected")) {
+                const link = document.createElement("link");
+                link.href = `/_addons/${StyleInjector.addonPackage}/user_files/field.css`;
+                link.type = "text/css";
+                link.rel = "stylesheet";
+                root.insertBefore(link, editable);
+                editable.setAttribute("has-css-injected", "");
+            }
+        });
     },
     refresh: async (notetype: string, mid: string): Promise<void> => {
         if (!StyleInjector.initialized) await StyleInjector.init("");
@@ -58,8 +43,7 @@ const StyleInjector = {
         document.documentElement.setAttribute("notetype", notetype);
         document.documentElement.setAttribute("mid", mid);
 
-        StyleInjector.roots.forEach((root) => {
-            const editable = root.querySelector("anki-editable") as HTMLElement;
+        StyleInjector.editables.forEach((editable) => {
             editable.setAttribute("notetype", notetype);
             editable.setAttribute("mid", mid);
         });
--- ts/src/utils.ts
+++ ts/src/utils.ts
@@ -1,11 +1,29 @@
 import * as NoteEditor from "anki/NoteEditor";
-import type { EditorFieldAPI } from "@anki/editor/EditorField.svelte";
+import { get } from "svelte/store";
+import type { PlainTextInputAPI } from "@anki/editor/plain-text-input";
+import type { RichTextInputAPI } from "@anki/editor/rich-text-input";
 
-export async function shadowRoots(): Promise<ShadowRoot[]> {
-    return await Promise.all(
-        NoteEditor.instances[0]?.fields.map(async (field: EditorFieldAPI) => {
-            const element = await field.element;
-            return element?.querySelector(".rich-text-editable")?.shadowRoot;
-        }),
-    );
+/**
+ * @author Hikaru Yoshiga <github.com/hikaru-y/>
+ */
+
+function isRichTextInputApi(
+    input: RichTextInputAPI | PlainTextInputAPI,
+): input is RichTextInputAPI {
+    return input.name === "rich-text";
+}
+
+export async function* iterateAnkiEditables(): AsyncGenerator<HTMLElement> {
+    while (!NoteEditor.instances[0]?.fields?.length) {
+        await new Promise(requestAnimationFrame);
+    }
+    for (const fieldApi of NoteEditor.instances[0].fields) {
+        const inputs = get(fieldApi.editingArea.editingInputs) as (
+            | PlainTextInputAPI
+            | RichTextInputAPI
+        )[];
+        const richTextInputApi = inputs.find(isRichTextInputApi)!;
+        const ankiEditable = await richTextInputApi.element;
+        yield ankiEditable;
+    }
 }
