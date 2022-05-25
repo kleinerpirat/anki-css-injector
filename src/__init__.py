fef38007ce65fea1efa51820e672bac86cea59cb
Matthias Metelka <62722460+kleinerpirat@users.noreply.github.com>
2022-05-25 17:46:45 +0200

    remove redundant addcards.py for backwards compatibility
    add_cards_did_change_note_type was only available in newer versions and editor_did_load_note is sufficient anyway

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,6 @@
 from .webview import init_webview
 from .editor import init_editor
-from .addcards import init_addcards
 
 def init() -> None:
     init_webview()
     init_editor()
-    init_addcards()
